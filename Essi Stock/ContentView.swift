c6079a373ee92e0090538fc4a44dabffcf882678
Adrien <62828317+adrien1020@users.noreply.github.com>
2022-06-15 00:21:12 +0200

    Added custom tab view

--- Essi Stock/ContentView.swift
+++ Essi Stock/ContentView.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedTab = "house.fill"
+    @Environment(\.colorScheme) var colorScheme
+    var iconName = ["house.fill","person.fill","magnifyingglass"]
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        CategoritesView()
-            .ignoresSafeArea(.keyboard)
+        VStack(spacing: 0){
+            TabView(selection: $selectedTab) {
+                CategoritesView()
+                    .tag(iconName[0])
+                CategoritesView()
+                    .tag(iconName[1])
+                CategoritesView()
+                    .tag(iconName[2])
+            }
+            Divider()
+            HStack{
+                ForEach(iconName, id: \.self) { image in
+                    Spacer()
+                    TabButton(selectedTab: $selectedTab, image: image)
+                    Spacer()
+                }
+            }
+            .background(colorScheme == .dark ? Color.black : Color.white)
+        }
+        .ignoresSafeArea(.keyboard)
     }
 }
+
+struct TabButton: View {
+    
+    @Binding var selectedTab: String
+    
+    var image: String
+    
+    var body: some View {
+        Button(action: {
+            selectedTab = image
+        }, label: {
+            Image(systemName: image)
+                .font(.system(size: 25))
+                .foregroundColor(selectedTab == image ? Color("Orange Color") : .black.opacity(0.4))
+                .padding()
+        })
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Essi Stock/Essi_StockApp.swift
+++ Essi Stock/Essi_StockApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct Essi_StockApp: App {
+    
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onChange(of: scenePhase, perform: {scene in
+                    switch scene{
+                    case .active:
+                        print("DEBUG: Scene is active")
+                    case .background:
+                        print("DEBUG: Scene is in background")
+                    case .inactive:
+                        print("DEBUG: Scene is inactive")
+                    default: print("DEBUG: Scene is unknow")
+                    }
+                })
         }
     }
 }
