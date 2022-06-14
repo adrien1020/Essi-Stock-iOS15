eea0ea3e1807d72016704f6b59eef6600da07961
Adrien <62828317+adrien1020@users.noreply.github.com>
2022-06-14 21:56:16 +0200

    Initial Commit

---
+++ Essi Stock/Essi_StockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Essi_StockApp.swift
+//  Essi Stock
+//
+//  Created by Adrien Surugue on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Essi_StockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
